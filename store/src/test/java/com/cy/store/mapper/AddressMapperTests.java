06e39b691a11a22ddc4e3c25759a6b6b23d084b7
MENGCHUNONE <117920680+MENGCHUNONE@users.noreply.github.com>
2024-12-14 15:51:04 +0800

    AddressMapperTests

--- store/src/test/java/com/cy/store/mapper/AddressMapperTests.java
+++ store/src/test/java/com/cy/store/mapper/AddressMapperTests.java
@@ -1,4 +1,34 @@
 package com.cy.store.mapper;
 
+import com.cy.store.entity.Address;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class AddressMapperTests {
+    @Autowired
+    private AddressMapper addressMapper;
+
+    @Test
+    public void insert() {
+        Address address = new Address();
+        address.setUid(18);
+        address.setName("Amy");
+        address.setPhone("17858802974");
+        address.setAddress("华南师范大学");
+        Integer rows = addressMapper.insert(address);
+        System.out.println("rows=" + rows);
+    }
+
+    @Test
+    public void countByUid() {
+        Integer uid = 18;
+        Integer count = addressMapper.countByUid(uid);
+        System.out.println("count=" + count);
+    }
 }
