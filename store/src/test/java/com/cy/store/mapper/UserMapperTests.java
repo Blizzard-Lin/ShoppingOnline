5cc6c6dd7d06d7d407c423910b2e4813b1305dbc
Blizzard <Blizzard_Queena@outlook.com>
2024-12-15 18:11:25 +0800

    解决问题

--- store/src/test/java/com/cy/store/mapper/UserMapperTests.java
+++ store/src/test/java/com/cy/store/mapper/UserMapperTests.java
@@ -1,7 +1,7 @@
 package com.cy.store.mapper;
 
 import com.cy.store.entity.User;
-//import com.cy.store.mapper.UserMapper;
+import com.cy.store.mapper.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
